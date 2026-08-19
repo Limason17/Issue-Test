52eadaa00bc03ed2636dd6caf21ba14a8eb6c78c
Dmytro <dmytro_yermakov@sluz.ch>
2026-08-19 11:34:38 +0200

    Fixes #6 : Navigation hinzugefügt

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <script setup>
+import { ref } from 'vue'
+import Navigation from './components/Navigation.vue'
 import Startpage from './components/Startpage.vue'
+import Personenkarte from './components/Personenkarte.vue'
+
+const currentTab = ref('start')
 </script>
 
 <template>
-  <Startpage />
+  <div class="app-layout">
+    <Navigation :active-tab="currentTab" @update:active-tab="currentTab = $event" />
+    <main class="main-content">
+      <Startpage v-if="currentTab === 'start'" />
+      <Personenkarte v-else-if="currentTab === 'team'" />
+    </main>
+  </div>
 </template>
+
+<style scoped>
+.app-layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100%;
+}
+
+.main-content {
+  flex: 1;
+  width: 100%;
+}
+</style>
