283158e2ac9a57a4d2a5312e3ca8585dcb6b4c88
bobiib <bobib0681@gmail.com>
2026-08-19 11:16:22 +0200

    Startseite erstellt #1

---
+++ src/components/Startpage.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const title = ref('Willkommen zu unserem Projekt')
+const description = ref('Dies ist die Startseite unseres GitHub-Projekts.')
+</script>
+
+<template>
+  <div class="startpage">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<style scoped>
+.startpage {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 80vh;
+  text-align: center;
+  padding: 20px;
+}
+</style>
